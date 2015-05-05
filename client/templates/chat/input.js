b74d5b28b4bb0bbe76020bd3f94d88778c076e12
balibou <benjamin.cherion@gmail.com>
2015-05-05 12:22:23 +0200

    refactoring test with no session and routes improvement

--- client/templates/chat/input.js
+++ client/templates/chat/input.js
@@ -7,11 +7,13 @@ Template.input.helpers({
 });
 
 Template.input.events({
-  'submit form' : function (e) {
-    e.preventDefault();
+  'submit form' : function (e,template) {
+    e.preventDefault(); //ne marche pas ?
 
     var message ={
       text: e.target.message.value,
+      roomId: Session.get("roomid")
+      // roomId: template.data._id
     };
 
     Meteor.call('messageInsert', message, function(error, result) {
@@ -19,7 +21,7 @@ Template.input.events({
         return alert(error.reason);
       }
       var chatId = ChatRooms.update({"_id":Session.get("roomid")},{$push:{messages:result.message}});
-      console.log(chatId);
+      // var chatId = ChatRooms.update({"_id":roomId},{$push:{messages:result.message}});
     });
     
     e.target.message.value = "";
--- client/templates/chat/sidebar.js
+++ client/templates/chat/sidebar.js
@@ -8,7 +8,18 @@ Template.sidebar.events({
   'click .user':function(){
     // Session.set('currentId',this._id);
     // console.log(Session.get('currentId'));
+
     var userId = this._id;
+    
+    // var result=ChatRooms.findOne({chatIds:{$all:[userId,Meteor.userId()]}});
+    // if (result){
+    //   Router.go('chatroom', {_id: result._id});
+    // }
+    // else{
+    //   newRoom= ChatRooms.insert({chatIds:[userId , Meteor.userId()],messages:[]});
+    //   Router.go('chatroom', {_id: newRoom._id});
+    // }
+
     var res=ChatRooms.findOne({chatIds:{$all:[userId,Meteor.userId()]}});
     if(res){
       Session.set("roomid",res._id);
