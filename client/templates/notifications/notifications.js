15eaa5b5483b1be2b755dc803fcc6c18e6e153f3
balibou <benjamin.cherion@gmail.com>
2015-05-11 17:59:35 +0200

    bug on desktop notifications fixed

--- client/templates/notifications/notifications.js
+++ client/templates/notifications/notifications.js
@@ -1,27 +1,25 @@
+Template.notifications.onCreated(function () {
+
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+
+  var instance = this;
+  var query = Notifications.find({userId: Meteor.userId(), read: false});
+  instance.autorun(function () {
+    
+    query.observeChanges({
+      added: function(id, notification) {
+        var notification = new Notification('Notification', {
+          icon: '',
+          body: "Vous avez un nouveau message !"
+        });
+      }
+    });
+  });
+});
+
 Template.notifications.helpers({
   notificationCount: function(){
-
-    if (Notification.permission !== "granted")
-    Notification.requestPermission();  
-
-    var count = 0;
-    var query = Notifications.find({userId: Meteor.userId(), read: false});
-    (function() {
-      var initializing = true;
-      query.observeChanges({
-        addedBefore: function(id, notification, before) {
-          if (!initializing) {
-            console.log(count);
-            var notification = new Notification('Notification', {
-              icon: '',
-              body: "Vous avez un nouveau message !"
-            });
-          }
-        }
-      });
-      initializing = false;
-    })();
-
     return Notifications.find({userId: Meteor.userId(), read: false}).count();
   },
   notificationChatroomPath: function() {
